a1e0302437f1547c23a9a00f5297e9ca256861e9
Aziret06 <aziretkarimov06@gmail.com>
2024-06-13 23:22:40 +0600

    next commit

--- logic.py
+++ logic.py
@@ -1,1 +1,6 @@
-print('Test')
+from random import randint
+lst = []
+for i in range(1, 11):
+    lst.append(i)
+
+correct_number = randint(1, 10)
--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from random import randint, choice as random_element  # alias
-from termcolor import cprint
-from emoji import emojize
-# from decouple import config
+import logic
+from decouple import config
 
-print(randint(1, 6))
-print(random_element(['apple', 'pear', 'orange', 'banana']))
+default_bet = config('INIT_CAPITAL')
+my_cash = int(config('MY_MONEY', default=1000))
+attempts = config('ATTEMPTS')
 
-
-cprint("Hello, World!", "green", "on_red")
-print(emojize('Python is :thumbs_up:'))
-print('Hello from CMD!')
-
-print('End of code')
+while my_cash > 0:
+    answer = input("Would you like to play ? (y/n): ").lower()
+    if answer == 'n':
+        print(f"That is your cash: {my_cash}")
+        break
+    elif answer == 'y':
+        my_bet = int(input('How much money would you bet: '))
+        if my_bet > my_cash or my_bet < 0:
+            print('You should bet from 1 to 1000')
+            continue
+        chosen_number = int(input('What number would you like to bet on(from 1 to 10):' ))
+        if chosen_number > 10 or chosen_number < 1:
+            print('You must enter a number between 1 and 10')
+            continue
+        if chosen_number == logic.correct_number:
+            print('Congratulations!')
+            my_cash += my_bet * 2
+        else:
+            print('You lose')
+            my_cash -= my_bet
+    else:
+        print("You must enter y or n")
